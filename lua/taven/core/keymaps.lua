0ecba1f4f4188a54db9e244a09ccfd01b54fbd02
taventran <taventran@gmail.com>
2024-12-04 20:38:10 -0600

    Added key mapping to increase and decrease windows horizontally

--- lua/taven/core/keymaps.lua
+++ lua/taven/core/keymaps.lua
@@ -5,10 +5,12 @@ local keymap = vim.keymap -- for conciseness
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk"})
 
 -- Window management
-keymap.set("n", "<leader>sv", "<C-w>v", {desc = "Split window vertically"}) 
-keymap.set("n", "<leader>sh", "<C-w>s", {desc = "Split windows horizontally"}) 
-keymap.set("n", "<leader>se", "<C-w>=", {desc = "Make splits equal size"}) 
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", {desc = "Close current split"}) 
+keymap.set("n", "<leader>sv", "<C-w>v", {desc = "Split window vertically"})
+keymap.set("n", "<leader>sh", "<C-w>s", {desc = "Split windows horizontally"})
+keymap.set("n", "<leader>se", "<C-w>=", {desc = "Make splits equal size"})
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", {desc = "Close current split"})
+keymap.set("n", "<leader>s+", "<cmd>horizontal resize +2<cr>", {desc = "Increase window horizontally"})
+keymap.set("n", "<leader>s-", "<cmd>horizontal resize -2<cr>", {desc = "Decrease window horizontally"})
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", {desc = "Open new tab"})
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", {desc = "Close current tab"})
